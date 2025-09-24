26dc20c56ab4bf66f1757029b1be90d3a30f28e2
Jesus Saucedo <23470133@campeche.tecnm.mx>
2025-09-24 11:13:19 -0600

    primer commit del proyecto contadorManual

---
+++ ContadorManual/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace ContadorManual
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private int _conteo;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _conteo = 0;
+            ConteoLabel.Text = _conteo.ToString();
+        }
+
+
+        private void Button_Clicke(Object sender, EventArgs e)
+        {
+            _conteo++;
+            ConteoLabel.Text = _conteo.ToString();
+        }
+
+        private void Button_Clicke_1(Object sender, EventArgs e)
+        {
+            _conteo = 0;
+            ConteoLabel.Text = _conteo.ToString();
+
+        }
+
+    }
+}
